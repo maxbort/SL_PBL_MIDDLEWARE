91b7ebefc056a38c3cdcd7899709b0671fb3c8ab
maxbort <cjfgh2449@naver.com>
2023-01-17 01:59:56 +0900

    초기환경 세팅

---
+++ src/main/java/com/example/slmiddleware/SLmiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.example.slmiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SLmiddlewareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SLmiddlewareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slmiddleware/SLmiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmiddleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SLmiddlewareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
